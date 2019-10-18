8eab137ba91d620bac4b82ffef520a356a891ebf
con-cis <54837156+con-cis@users.noreply.github.com>
2019-10-18 11:53:25 +0200

    check folder and file new
    tested on powershell 6.1

--- copyandlog.ps1
+++ copyandlog.ps1
@@ -1,14 +1,51 @@
-# Clear log older than x-Days
 $Daysback = "-60"
+$path = "C:\Logs"
+$date = get-date -format "yyyy-MM-dd-HH-mm"
+$file = ("Log_" + $date + ".log")
+$logfile = $path + "\" + $file
+$logtext = $error
+$error.clear()
+
+# Write log to logpath
+function Write-Log([string]$logtext, [int]$level=0)
+{
+	$logdate = get-date -format "yyyy-MM-dd HH:mm:ss"
+	if($level -eq 0)
+	{
+		$logtext = "[INFO] " + $logtext
+		$text = "["+$logdate+"] - " + $logtext
+		Write-Host $text
+	}
+	if($level -eq 1)
+	{
+		$logtext = "[WARNING] " + $logtext
+		$text = "["+$logdate+"] - " + $logtext
+		Write-Host $text -ForegroundColor Yellow
+	}
+	if($level -eq 2)
+	{
+		$logtext = "[ERROR] " + $logtext
+		$text = "["+$logdate+"] - " + $logtext
+		Write-Host $text -ForegroundColor Red
+						  
+	}
+	$text >> $logfile
+	Clear-Log $Daysback
+}
+# Clear log older than x-Days
+function Clear-Log([int]$Daysback)
+{
+	$LogPath = "D:\Logs" 
+	$CurrentDate = Get-Date
+	$DatetoDelete = $CurrentDate.AddDays($Daysback)
+	Get-ChildItem $LogPath | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
+}
 try{
-    $folder = 'C:\YOUR_PATH[\NEW_FILE]'
-    $files = gci 'C:\YOUR_PATH\OLD' -Filter *.xml | sort LastWriteTime -Desc 
-    if ($files){
+    $folder = gci 'C:\[YOURPATH]\[YOURFILE_NEW]'
+    $files = gci 'C:\[YOURPATH_OLD]' -Filter *.xml | sort LastWriteTime -Desc 
+    if ($files -and $folder){
         $files | select -First 1 | copy-item -Destination $folder -Force
         Write-Log "Success copying Backup XML"
-    }
-    elseif (!$files){
-        throw "File is NULL"
     }
     else {
         throw $error
@@ -16,46 +53,4 @@ try{
 }
 catch{
     Write-Log "Error while copying Backup File: $error" 2
-}
-finally{
-    $path = "C:\Temp"
-    $date = get-date -format "yyyy-MM-dd-HH-mm-ss"
-    $file = ("Log_" + $date + ".log")
-    $logfile = $path + "\" + $file
-    $logtext = $error
-    $error.clear()
-
-    function Write-Log([string]$logtext, [int]$level=0)
-    {
-	    $logdate = get-date -format "yyyy-MM-dd HH:mm:ss"
-	    if($level -eq 0)
-	    {
-		    $logtext = "[INFO] " + $logtext
-		    $text = "["+$logdate+"] - " + $logtext
-		    Write-Host $text
-	    }
-	    if($level -eq 1)
-	    {
-		    $logtext = "[WARNING] " + $logtext
-		    $text = "["+$logdate+"] - " + $logtext
-		    Write-Host $text -ForegroundColor Yellow
-	    }
-	    if($level -eq 2)
-	    {
-		    $logtext = "[ERROR] " + $logtext
-		    $text = "["+$logdate+"] - " + $logtext
-		    Write-Host $text -ForegroundColor Red
-                              
-	    }
-	    $text >> $logfile
-        Clear-Log $Daysback
-    }
-    
-    function Clear-Log([int]$Daysback)
-    {
-        $LogPath = "C:\temp" 
-        $CurrentDate = Get-Date
-        $DatetoDelete = $CurrentDate.AddDays($Daysback)
-        Get-ChildItem $LogPath | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
-    }
 }
